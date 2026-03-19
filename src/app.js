bcf27dc8ea6c299524652e6d79b9422f22c8c771
santosh04254G <santhoshadithya2020@gmail.com>
2026-03-19 18:53:27 +0530

    created a server

--- src/app.js
+++ src/app.js
@@ -10,11 +10,19 @@ app.use("/test",(req,res)=>{  //(/test is the url route)
     res.send("helloo")
 });
 
-app.use("/me",(req,res)=>{
+app.get("/user",(req,res)=>{
 
-    res.send("its me");
+    res.send({firstname:"santosh",lastname:"aditya"});
 
-})
+});
+
+app.post("/user",(req,res)=>{
+    res.send("deleted succesfully");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted succesfully");
+});
 
 app.listen(3000,()=>{
     console.log("server is succesfully listening on this port");
